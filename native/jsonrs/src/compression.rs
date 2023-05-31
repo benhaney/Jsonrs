91e629c630e3594e82c07a6c7bc2440a3db45d53
Ben Haney <ben@benhaney.com>
2023-05-30 22:25:55 -0500

    Add other flate formats

--- native/jsonrs/src/compression.rs
+++ native/jsonrs/src/compression.rs
@@ -1,12 +1,14 @@
 use rustler::NifUnitEnum;
 use flate2::Compression;
-use flate2::write::GzEncoder;
+use flate2::write::{DeflateEncoder, GzEncoder, ZlibEncoder};
 use std::io::{BufWriter, Write, Error};
 
 #[derive(NifUnitEnum)]
 pub enum Algs {
   None,
+  Deflate,
   Gzip,
+  Zlib,
 }
 
 pub trait BufWrapper: Write {
@@ -17,18 +19,33 @@ impl BufWrapper for Vec<u8> {
   fn get_buf(&mut self) -> Result<Vec<u8>, Error> { Ok(self.to_vec()) }
 }
 
-impl BufWrapper for BufWriter<GzEncoder<Vec<u8>>> {
-  fn get_buf(&mut self) -> Result<Vec<u8>, Error> {
-    self.flush()?;
-    self.get_mut().try_finish()?;
-    Ok(self.get_ref().get_ref().to_vec())
+macro_rules! impl_BufWrapper {
+  (for $($t:ty),+) => {
+    $(impl BufWrapper for $t {
+      fn get_buf(&mut self) -> Result<Vec<u8>, Error> {
+        self.flush()?;
+        self.get_mut().try_finish()?;
+        Ok(self.get_ref().get_ref().to_vec())
+      }
+    })*
   }
 }
 
+impl_BufWrapper!(for
+  BufWriter<DeflateEncoder<Vec<u8>>>,
+  BufWriter<GzEncoder<Vec<u8>>>,
+  BufWriter<ZlibEncoder<Vec<u8>>>
+);
+
 pub fn get_writer(opts: Option<(Algs, Option<u32>)>) -> Box<dyn BufWrapper> {
+  let comp_lvl = match opts {
+    Some((_, Some(lvl))) => Compression::new(lvl),
+    _ => Compression::default(),
+  };
   match opts {
-    Some((Algs::Gzip, None)) => Box::new(BufWriter::with_capacity(10_240, GzEncoder::new(Vec::new(), Compression::default()))),
-    Some((Algs::Gzip, Some(lv))) => Box::new(BufWriter::with_capacity(10_240, GzEncoder::new(Vec::new(), Compression::new(lv)))),
+    Some((Algs::Deflate, _)) => Box::new(BufWriter::with_capacity(10_240, DeflateEncoder::new(Vec::new(), comp_lvl))),
+    Some((Algs::Gzip, _)) => Box::new(BufWriter::with_capacity(10_240, GzEncoder::new(Vec::new(), comp_lvl))),
+    Some((Algs::Zlib, _)) => Box::new(BufWriter::with_capacity(10_240, ZlibEncoder::new(Vec::new(), comp_lvl))),
     _ => Box::new(Vec::<u8>::new()),
   }
 }
